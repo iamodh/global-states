edac3fae66a61382bba09cfb72796fe3b8d7c02d
Donghwan Oh <imodh97@gmail.com>
2025-06-19 11:10:24 +0900

    Zustand 예제 추가

--- src/pages/Zustand.tsx
+++ src/pages/Zustand.tsx
@@ -1,3 +1,24 @@
+import { useBearStore } from "../libs/zustand/store";
+
 export default function Zustand() {
-  return <h1>Zustand</h1>;
+  const { bears, increase, reset } = useBearStore();
+
+  const onClick = () => {
+    increase(1);
+  };
+
+  const onReset = () => {
+    reset();
+  };
+
+  return (
+    <div>
+      <button onClick={onClick}>Increase Bears</button>
+      <button onClick={onReset}>Reset</button>
+      <br />
+      number: {bears}
+      <br />
+      {"🐻".repeat(bears)}
+    </div>
+  );
 }
