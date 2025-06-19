1095947b8c45d847cb24b88a11ea8e3431c90ba4
Donghwan Oh <imodh97@gmail.com>
2025-06-19 10:30:39 +0900

    홈 페이지 제작

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,18 @@
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <div>
+      <h1>상태관리 라이브러리 비교하기</h1>
+      <p>
+        상태관리 라이브러리 사용법을 익히고, 장단점에 대해 탐구해보기 위한
+        테스트 프로젝트입니다.
+      </p>
+      <hr />
+      <h4>버전 정보</h4>
+      <details>
+        <summary>
+          0.1.0 - recoil 페이지 추가 (React 19버전과 호환성 문제로 동작 안 함)
+        </summary>
+      </details>
+    </div>
+  );
 }
